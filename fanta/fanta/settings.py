c5b3bac1dc050fdb83b83dd94ab0fd5dfcb66584
abenassen <abenassen@users.noreply.github.com>
2017-08-11 17:49:37 +0200

    setting to work with sqlite locally

--- fanta/fanta/settings.py
+++ fanta/fanta/settings.py
@@ -93,21 +93,22 @@ if 'RDS_DB_NAME' in os.environ:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'fantadb',
-            'USER': 'postgres',
-            'PASSWORD': '66740811',
-            'HOST' : 'localhost',
-            'PORT' : '5432',
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
-    } 
+    }
+
+    #DATABASES = {
+    #    'default': {
+    #        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #        'NAME': 'fantadb',
+    #        'USER': 'postgres',
+    #        'PASSWORD': '66740811',
+    #        'HOST' : 'localhost',
+    #        'PORT' : '5432',
+    #    }
+    #} 
     
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
 
 
 # Internationalization
